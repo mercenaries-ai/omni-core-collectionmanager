da32fa929177a4d0b24dff895895d98bd368d594
Minhye Kim <popenaga@gmail.com>
2023-08-28 18:00:44 +0900

    Fix cursor bug

--- public/main.js
+++ public/main.js
@@ -6237,7 +6237,7 @@ var LoadMoreRenderer = class extends CollectionRenderer_default {
   render(content) {
     const opts = { cursor: content?.cursor };
     console.log(content, opts);
-    return `<div class="collection-${content.type} cursor-pointer" @click='fetchItems(${JSON.stringify({ opts })})'>
+    return `<div class="collection-${content.type} cursor-pointer" @click='fetchItems(${JSON.stringify(opts)})'>
     <div class="loadmore">
     <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" viewBox="0 0 80 80" fill="none">
 <path d="M40 6.40002V76.8M8 40H72" stroke="#DEDEDF" stroke-width="6.4" stroke-linecap="round" stroke-linejoin="round"/>
--- src/client/renderers/LoadMoreRenderer.ts
+++ src/client/renderers/LoadMoreRenderer.ts
@@ -4,7 +4,7 @@ class LoadMoreRenderer extends CollectionRenderer {
   render(content: any): string {
     const opts = {cursor: content?.cursor};
     console.log(content, opts)
-    return `<div class="collection-${content.type} cursor-pointer" @click='fetchItems(${JSON.stringify({opts: opts})})'>
+    return `<div class="collection-${content.type} cursor-pointer" @click='fetchItems(${JSON.stringify(opts)})'>
     <div class="loadmore">
     <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" viewBox="0 0 80 80" fill="none">
 <path d="M40 6.40002V76.8M8 40H72" stroke="#DEDEDF" stroke-width="6.4" stroke-linecap="round" stroke-linejoin="round"/>
